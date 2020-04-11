2b9610c2f3ddd2116668b298eee9d39e8644c329
Parth <parthpandya.utsc@gmail.com>
2020-04-11 11:44:29 -0400

    Update sp500.py

--- sp500.py
+++ sp500.py
@@ -9,6 +9,10 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
 # import fix_yahoo_finance as yf
+from collections import Counter
+from sklearn import svm, neighbors
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 
 # yf.pdr_override
 
@@ -150,9 +154,47 @@ def buy_sell_hold(*args):
         
 def extract_featuresets(ticker):
     tickers, df = process_data_for_labels(ticker)
+    #creates a new column ticker_target that contains either 1, -1, or 0
+    df['{}_target'.format(ticker)] = list(map(buy_sell_hold, df['{}_1d'.format(ticker)], df['{}_2d'.format(ticker)], df['{}_3d'.format(ticker)], df['{}_4d'.format(ticker)], df['{}_5d'.format(ticker)], df['{}_6d'.format(ticker)], df['{}_7d'.format(ticker)]))
     
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_values = [str(i) for i in vals]
+    print('Data spread:', Counter(str_values))
+                                              
+    df.fillna(0, inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+    
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+    
+    X = df_vals.values
+    Y = df['{}_target'.format(ticker)].values
 
+    return X, Y, df
+
+def do_ml(ticker):
+    X, Y, df = extract_featuresets(ticker)
     
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5)
+    
+    #clf = neighbors.KNeighborsClassifier()
+    clf = VotingClassifier([('lsvc', svm.LinearSVC()),
+                            ('knn', neighbors.KNeighborsClassifier()),
+                            ('rfor', RandomForestClassifier())])
+    
+    clf.fit(X_train, Y_train)
+    confidence = clf.score(X_test, Y_test)
+    
+    print('Accuracy', confidence)
+    
+    predictions = clf.predict(X_test)
+    print('Predicted spread:', Counter(predictions))
+    
+    return confidence
+
+do_ml('AAP')
 
         
         
